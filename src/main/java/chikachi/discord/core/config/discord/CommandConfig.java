40879cde21dae30ada454524501dd98014d2f21d
Aaron Rinn <arinn1204@users.noreply.github.com>
2024-09-02 10:21:04 -0700

    default enabled to true for commands

--- src/main/java/chikachi/discord/core/config/discord/CommandConfig.java
+++ src/main/java/chikachi/discord/core/config/discord/CommandConfig.java
@@ -33,7 +33,7 @@ public class CommandConfig {
     private static final Logger log = LoggerFactory.getLogger(CommandConfig.class);
     private String name;
     private String command;
-    private boolean enabled;
+    private boolean enabled = true;
     private List<String> aliases = new ArrayList<>();
     private List<String> permissions = new ArrayList<>();
 
